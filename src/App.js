9f4282f7934265d3b29ddbb9f855ce47a3ee44cc
Nicholas Chen <grahmnic@gmail.com>
2020-05-04 21:11:11 -0400

    Initializing repository

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="root">
+      <div className="header">
+      </div>
+      <div className="content">
+      </div>
+      <div className="footer">
+      </div>
+    </div>
+  );
+}
+
+export default App;
